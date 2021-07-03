800753ead4f65eea6e596e11f475d11872803a29
prox2021 <proxisoft2021@gmail.com>
2021-07-03 23:16:22 +0100

    création la structure de projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AboutComponent } from './components/son/about/about.component';
+import { ContactComponent } from './components/son/contact/contact.component';
+import { CvComponent } from './components/son/cv/cv.component';
+import { EducationComponent } from './components/son/education/education.component';
+import { ProjetComponent } from './components/son/projet/projet.component';
+import { DetailsProjetComponent } from './components/son/projet/details-projet/details-projet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactComponent,
+    CvComponent,
+    EducationComponent,
+    ProjetComponent,
+    DetailsProjetComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/son/projet/details-projet/details-projet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-projet',
+  templateUrl: './details-projet.component.html',
+  styleUrls: ['./details-projet.component.scss']
+})
+export class DetailsProjetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
