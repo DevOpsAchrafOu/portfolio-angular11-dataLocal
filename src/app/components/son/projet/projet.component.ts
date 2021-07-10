0cce17bd65600c683479aa9375d7e9439a016424
prox2021 <proxisoft2021@gmail.com>
2021-07-10 21:55:57 +0100

    fait component de la liste des Projets

--- src/app/components/son/projet/projet.component.ts
+++ src/app/components/son/projet/projet.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ProjetService} from '../../../services/projet.service';
 
 @Component({
   selector: 'app-projet',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjetComponent implements OnInit {
 
-  constructor() { }
+  /********************************************************************************************/
+  /**************************************** les Attributes  ***********************************/
+  /********************************************************************************************/
+
+  data: string = 'projet';
+  @Output() dataEvent = new EventEmitter<string>();
+  projets: any;
+
+
+  /********************************************************************************************/
+  /********************************* les fonctions d'intialisations  **************************/
+  /********************************************************************************************/
+
+  constructor(private projetService: ProjetService) { }
 
   ngOnInit(): void {
+    this.sendData();
+    this.projets = this.projetService.getProjets();
+  }
+
+
+  /********************************************************************************************/
+  /**************************************  les fonctions **************************************/
+  /********************************************************************************************/
+
+  sendData(): void {
+    this.dataEvent.emit(this.data);
   }
 
 }
