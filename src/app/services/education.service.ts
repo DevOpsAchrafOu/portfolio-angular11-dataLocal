25fccc1dde65f110483fff60dc31d43e17dfc94c
prox2021 <proxisoft2021@gmail.com>
2021-07-10 21:47:06 +0100

    fait service de Education

--- src/app/services/education.service.ts
+++ src/app/services/education.service.ts
@@ -1,9 +1,61 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import education from '../../assets/dataLocal/education.json';
+import formation from '../../assets/dataLocal/formation.json';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EducationService {
 
-  constructor() { }
+  /********************************************************************************************/
+  /**************************************** les Attributes  ***********************************/
+  /********************************************************************************************/
+
+  private educations:
+    {
+      id: number,
+      duration: string,
+      name: string,
+      university: string,
+      other: string
+    }[] = education;
+
+  private formation:
+    {
+      id: number,
+      duration: string,
+      type: string,
+      description: string,
+      other: string
+    }[] = formation;
+
+  /********************************************************************************************/
+  /********************************* les fonctions d'intialisations  **************************/
+
+  /********************************************************************************************/
+
+  constructor() {
+  }
+
+  /********************************************************************************************/
+  /**************************************  les fonctions **************************************/
+  /********************************************************************************************/
+
+
+  getEducations(): any {
+    return this.educations;
+  }
+
+  getEducation(id: number): any {
+    return this.educations.find((h: any) => h.id === id);
+  }
+
+  getFormations(): any {
+    return this.formation;
+  }
+
+  getFormation(id: number): any {
+    return this.formation.find((h: any) => h.id === id);
+  }
 }
