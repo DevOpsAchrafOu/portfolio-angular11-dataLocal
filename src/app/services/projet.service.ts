84b794144f5628ab9ba2ee58fdd4eea5231b85e1
prox2021 <proxisoft2021@gmail.com>
2021-07-10 21:54:55 +0100

    fait service de Projet

--- src/app/services/projet.service.ts
+++ src/app/services/projet.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import projet from '../../assets/dataLocal/projet.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjetService {
 
-  constructor() { }
+  /********************************************************************************************/
+  /****************************** les Attributes et les Données *******************************/
+  /********************************************************************************************/
+
+
+  private projets: {
+    id: number,
+    duration: string,
+    name: string,
+    titre: string,
+    description: string,
+    titreTache: string,
+    tache: string,
+    technologies: string,
+    show: number,
+    images: {id: number, name: string, description: string, url: string}[],
+    taches: {id: number, name: string, description: string}[],
+    urlGit: []
+  }[] = projet;
+
+
+  /********************************************************************************************/
+  /********************************* les fonctions d'intialisations  **************************/
+  /********************************************************************************************/
+
+  constructor() {
+  }
+
+  /********************************************************************************************/
+  /**************************************  les fonctions **************************************/
+  /********************************************************************************************/
+
+
+  getProjets(): any {
+    return this.projets;
+  }
+
+  getProjet(id: number): any {
+    return this.projets.find((p: any) => p.id === id);
+  }
+
+
 }
