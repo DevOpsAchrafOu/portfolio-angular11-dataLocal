82675a4ca6ee56a4f6c8e3ab9ae8f5f8a7e3fb4f
prox2021 <proxisoft2021@gmail.com>
2021-07-31 20:54:12 +0100

    show projet in heroku

--- src/app/components/son/projet/details-projet/details-projet.component.ts
+++ src/app/components/son/projet/details-projet/details-projet.component.ts
@@ -3,11 +3,15 @@ import {ProjetService} from '../../../../services/projet.service';
 import {ActivatedRoute} from '@angular/router';
 import {formatNumber} from '@angular/common';
 import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
+import {NgbCarousel, NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-details-projet',
   templateUrl: './details-projet.component.html',
-  styleUrls: ['./details-projet.component.scss']
+  styleUrls: ['./details-projet.component.scss'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
+
 })
 export class DetailsProjetComponent implements OnInit {
 
@@ -21,21 +25,36 @@ export class DetailsProjetComponent implements OnInit {
   private modalReference: any;
   idPicture: any;
 
+  //for show image
+  showNavigationArrows = true;
+  showNavigationIndicators = true;
+  images: any;
+  indexCurrent = 2;
+
+
   /********************************************************************************************/
   /********************************* les fonctions d'intialisations  **************************/
-
   /********************************************************************************************/
   constructor(private projetService: ProjetService,
               private route: ActivatedRoute,
               private modalService: NgbModal,
-              private config: NgbModalConfig/*pour popup*/
+              private config: NgbModalConfig,/*pour popup*/
+              carouselConfigconfig: NgbCarouselConfig
   ) {
+    // customize default values of carousels used by this component tree
+    carouselConfigconfig.showNavigationArrows = true;
+    carouselConfigconfig.showNavigationIndicators = true;
   }
+
 
   ngOnInit(): void {
     this.getIdParam();
   }
 
+  changeCurrentImg(i: any): void {
+    this.indexCurrent = i;
+    console.log("indexCurrent : "+this.indexCurrent);
+  }
 
   /********************************************************************************************/
   /**************************************  les fonctions **************************************/
@@ -58,6 +77,8 @@ export class DetailsProjetComponent implements OnInit {
 
       if (idProjet != null) {
         this.projet = this.projetService.getProjet(Number(idProjet));
+        this.images = this.projet.images;
+        console.log(this.images);
       }
 
     });
