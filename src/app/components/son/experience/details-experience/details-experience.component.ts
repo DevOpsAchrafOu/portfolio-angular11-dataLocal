2b373935922939fea2c7dddccbef003654851dfc
prox2021 <proxisoft2021@gmail.com>
2021-11-08 20:22:18 +0100

    changer cv

--- src/app/components/son/experience/details-experience/details-experience.component.ts
+++ src/app/components/son/experience/details-experience/details-experience.component.ts
@@ -14,6 +14,7 @@ export class DetailsExperienceComponent implements OnInit {
   /**************************************** les Attributes  ***********************************/
   /********************************************************************************************/
 
+
   experience: any;
   // popup pour affichier les images
   @ViewChild('content', {static: false}) content: any;
